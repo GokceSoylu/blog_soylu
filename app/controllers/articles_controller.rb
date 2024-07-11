f2581b8dca0a75070622bb880c9f13fe01b4e9da
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-07-11 19:27:56 +0300

    show action and it's html page added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,10 @@ class ArticlesController < ApplicationController
     @articles=Article.all
   end
 
+  def show
+    @article=Article.find(params[:id])
+  end
+
   private
   def article_params
     params.require(:article).permit(:title,:text)
